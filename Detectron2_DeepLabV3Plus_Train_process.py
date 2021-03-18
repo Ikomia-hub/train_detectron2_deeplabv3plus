3851a9d16ea7aa739cc40bde3c04955217a5bcc0
ambroiseb <43398946+ambroiseb@users.noreply.github.com>
2021-03-18 13:49:49 +0100

    training with predifined parameters, mlflow visualisation

--- Detectron2_DeepLabV3Plus_Train_process.py
+++ Detectron2_DeepLabV3Plus_Train_process.py
@@ -4,9 +4,7 @@ from ikomia.dnn import dataset, datasetio
 from detectron2.config import get_cfg
 from ikomia.dnn import dnntrain
 import deeplabutils
-import cv2
 import os
-from detectron2.data import MetadataCatalog, DatasetCatalog
 from detectron2.config import get_cfg
 from detectron2 import model_zoo
 from detectron2.engine import DefaultTrainer,DefaultPredictor
@@ -27,7 +25,7 @@ class Detectron2_DeepLabV3Plus_TrainParam(dataprocess.CDnnTrainProcessParam):
         dataprocess.CDnnTrainProcessParam.__init__(self)
         # Place default value initialization here
         self.cfg = get_cfg()
-        self.inputSize=(512,512)
+        self.inputSize=(800,800)
         self.numClasses=3
         self.maxIter = 100
         self.warmupFactor = 0.001
@@ -116,56 +114,52 @@ class Detectron2_DeepLabV3Plus_TrainProcess(dnntrain.TrainProcess):
 
         # Get parameters :
         param = self.getParam()
-        DatasetCatalog.clear()
-        DatasetCatalog.register("datasetTrain", deeplabutils.my_dataset_function(input.data))
 
-        if param.ignoreValue is not None:
-            MetadataCatalog.get("datasetTrain").ignore_label = param.ignoreValue
         cfg = get_cfg()
         add_deeplab_config(cfg)
         cfg.merge_from_file(os.path.dirname(os.path.realpath(__file__))+"/model/configs/deeplab_v3_plus_R_103_os16_mg124_poly_90k_bs16.yaml")
         cfg.DATASETS.TRAIN = ("datasetTrain",)
-        cfg.DATASETS.TEST = ()
-        cfg.SOLVER.MAX_ITER = 100
+        cfg.DATASETS.TEST = ("datasetTest",)
+        cfg.SOLVER.MAX_ITER = 200
         cfg.SOLVER.WARMUP_FACTOR = 0.001
-        cfg.SOLVER.WARMUP_ITERS = 40
+        cfg.SOLVER.WARMUP_ITERS = 50
         cfg.SOLVER.POLY_LR_FACTOR = 0.9
         cfg.SOLVER.POLY_LR_CONSTANT_FACTOR = 0.0
         cfg.MODEL.SEM_SEG_HEAD.NUM_CLASSES = 3
         cfg.SOLVER.BASE_LR = 0.02
         cfg.MODEL.SEM_SEG_HEAD.ASPP_CHANNELS = 256
         cfg.MODEL.SEM_SEG_HEAD.COMMON_STRIDE = 4
-        cfg.SOLVER.IMS_PER_BATCH=1
-        cfg.DATALOADER.NUM_WORKERS = 2
-        cfg.inputSize=param.inputSize
+        cfg.SOLVER.IMS_PER_BATCH=2
+        cfg.DATALOADER.NUM_WORKERS = 0
+        cfg.INPUT_SIZE=param.inputSize
+        cfg.TEST.EVAL_PERIOD = 20
+        cfg.PATIENCE = 10
 
+        deeplabutils.register_train_test(input.data["images"],input.data["metadata"],test_ratio=0.33,seed=0)
         cfg.OUTPUT_DIR = os.path.dirname(os.path.realpath(__file__))+"/output"
         os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 
         if len(input.data["images"])>0:
-            trainer = deeplabutils.MyTrainer(cfg)
-            trainer.resume_or_load(resume=False)
-            trainer.train()
-        # Get output :
-        # output = self.getOutput(indexOfOutput)
-
-
-
-        # Get image from input/output (numpy array):
-        # srcImage = input.getImage()
-
-        # Call to the process main routine
-        # dstImage = ...
-
-        # Set image of input/output (numpy array):
-        # output.setImage(dstImage)
-
-        # Step progress bar:
-        self.emitStepProgress()
-
+            self.trainer = deeplabutils.MyTrainer(cfg,self)
+            self.trainer.resume_or_load(resume=False)
+            print("Starting training job...")
+            self.trainer.train()
+            print("Training job finished.")
         # Call endTaskRun to finalize process
         self.endTaskRun()
 
+    def getProgressSteps(self, eltCount=1):
+        # Function returning the number of progress steps for this process
+        # This is handled by the main progress bar of Ikomia application
+        param = self.getParam()
+        if param is not None:
+            return param.maxIter
+        else:
+            return 1
+
+    def stop(self):
+        super().stop()
+        self.trainer.run=False
 
 # --------------------
 # - Factory class to build process object
--- deeplabutils.py
+++ deeplabutils.py
@@ -7,11 +7,22 @@ import logging
 
 import detectron2.data.transforms as T
 from detectron2.data import DatasetMapper
-from detectron2.data import build_detection_train_loader
+from detectron2.data import build_detection_train_loader, build_detection_test_loader
 import copy
 from detectron2.data import detection_utils as utils
 from typing import List, Union
-
+from detectron2.utils.events import EventStorage
+import logging
+from detectron2.evaluation import SemSegEvaluator
+import PIL.Image as Image
+from detectron2.utils.file_io import PathManager
+from detectron2.engine.hooks import HookBase
+from detectron2.utils.logger import log_every_n_seconds
+import detectron2.utils.comm as comm
+import time
+import datetime
+from detectron2.data import MetadataCatalog, DatasetCatalog
+import random
 
 def my_dataset_function(ikDataset):
     #returns a function that returns a list[dict] to be used as detectron2 dataset
@@ -31,7 +42,7 @@ def my_dataset_function(ikDataset):
             record["height"]=ikrecord["height"]
             record["width"]=ikrecord["width"]
             if category_colors is not None:
-                record["category_colors"]=category_colors
+                record["category_colors"]={v: k for k, v in category_colors.items()}
             listDict.append(record)
         return listDict
     return f
@@ -49,23 +60,182 @@ def rgb2mask(img, color2index):
             mask[img_id == c] = color2index[tuple(img[img_id == c][0])]
         except:
             pass
+    np.unique(mask)
+    np.unique(img.reshape(-1, img.shape[2]), axis=0)
+
     return mask
 
+class LossEvalHook(HookBase):
+    def __init__(self, eval_period, model, data_loader, trainer, train_process, patience):
+        self._model = model
+        self._period = eval_period
+        self._data_loader = data_loader
+        self.best_val_loss= np.inf
+        self.patience = patience
+        self.waiting = 0
+        self.trainer=trainer
+        self.train_process=train_process
+
+    def _do_loss_eval(self):
+        # Copying inference_on_dataset from evaluator.py
+        total = len(self._data_loader)
+        num_warmup = min(5, total - 1)
+
+        start_time = time.perf_counter()
+        total_compute_time = 0
+        losses = []
+        for idx, inputs in enumerate(self._data_loader):
+            if idx == num_warmup:
+                start_time = time.perf_counter()
+                total_compute_time = 0
+            start_compute_time = time.perf_counter()
+            if torch.cuda.is_available():
+                torch.cuda.synchronize()
+            total_compute_time += time.perf_counter() - start_compute_time
+            iters_after_start = idx + 1 - num_warmup * int(idx >= num_warmup)
+            seconds_per_img = total_compute_time / iters_after_start
+            if idx >= num_warmup * 2 or seconds_per_img > 5:
+                total_seconds_per_img = (time.perf_counter() - start_time) / iters_after_start
+                eta = datetime.timedelta(seconds=int(total_seconds_per_img * (total - idx - 1)))
+                log_every_n_seconds(
+                    logging.INFO,
+                    "Loss on Validation  done {}/{}. {:.4f} s / img. ETA={}".format(
+                        idx + 1, total, seconds_per_img, str(eta)
+                    ),
+                    n=5,
+                )
+            loss_batch = self._get_loss(inputs)
+            losses.append(loss_batch)
+        mean_loss = np.mean(losses)
+        if mean_loss<self.best_val_loss:
+            self.best_val_loss=mean_loss
+            self.waiting=0
+        self.waiting+=1
+
+        self.trainer.storage.put_scalar('validation_loss', mean_loss)
+
+        metrics_dict = {k:v[0] for k,v in self.trainer.storage.latest().items()}
+        metrics_dict["nb_iter"]=self.trainer.storage.latest()["time"][1]
+        self.train_process.log_metrics(metrics_dict)
+
+        if self.waiting>self.patience:
+            self.trainer.run=False
+        comm.synchronize()
+
+        # simple early stopping criteria
+        return losses
+
+    def _get_loss(self, data):
+        # How loss is calculated on train_loop
+        metrics_dict = self._model(data)
+        metrics_dict = {
+            k: v.detach().cpu().item() if isinstance(v, torch.Tensor) else float(v)
+            for k, v in metrics_dict.items()
+        }
+        total_losses_reduced = sum(loss for loss in metrics_dict.values())
+        return total_losses_reduced
+
+    def after_step(self):
+        next_iter = self.trainer.iter + 1
+        is_final = next_iter == self.trainer.max_iter
+        if is_final or (self._period > 0 and next_iter % self._period == 0):
+            self._do_loss_eval()
+        self.trainer.storage.put_scalars(timetest=12)
 
 class MyTrainer(DefaultTrainer):
-    def __init__(self, cfg):
+    def __init__(self, cfg, train_process):
         """
         Args:
             cfg (CfgNode):
         """
+        self.run=True
+        self.train_process=train_process
         super().__init__(cfg)
+
 
     def build_lr_scheduler(cls, cfg, optimizer):
         return build_deeplab_lr_scheduler(cfg, optimizer)
 
     def build_train_loader(cls, cfg):
-        return build_detection_train_loader(cfg, mapper=MyMapper(True, augmentations=[T.Resize(cfg.inputSize)], image_format="RGB"))
+        return build_detection_train_loader(cfg, mapper=MyMapper(True, augmentations=[T.Resize(cfg.INPUT_SIZE)], image_format="RGB"))
 
+    def build_evaluator(cfg, dataset_name):
+        return MySemSegEvaluator(dataset_name, distributed=False, output_dir="eval", num_classes=3)
+
+    def build_hooks(self):
+        hooks = super().build_hooks()
+        hooks.insert(-1, LossEvalHook(
+            self.cfg.TEST.EVAL_PERIOD,
+            self.model,
+            build_detection_test_loader(
+                self.cfg,
+                self.cfg.DATASETS.TEST[0],
+                MyMapper(False, augmentations=[T.Resize(self.cfg.INPUT_SIZE)], image_format="RGB")
+            ),
+            self,
+            self.train_process,
+            self.cfg.PATIENCE
+        ))
+        return hooks
+
+    def train(self):
+
+        logger = logging.getLogger(__name__)
+        logger.info("Starting training from iteration {}".format(self.start_iter))
+
+        with EventStorage(self.start_iter) as self.storage:
+            try:
+                self.before_train()
+                for self.iter in range(self.start_iter, self.max_iter):
+                    if not(self.run):
+                        break
+                    self.before_step()
+                    self.run_step()
+                    self.after_step()
+                    self.train_process.emitStepProgress()
+                # self.iter == max_iter can be used by `after_train` to
+                # tell whether the training successfully finished or failed
+                # due to exceptions.
+                self.iter += 1
+            except Exception:
+                logger.exception("Exception during training:")
+                raise
+            finally:
+                self.after_train()
+
+class MySemSegEvaluator(SemSegEvaluator):
+    """
+    Evaluate semantic segmentation metrics.
+    """
+
+    def __init__(self, dataset_name, distributed, num_classes, ignore_label=255, output_dir=None):
+        super().__init__(dataset_name, distributed, num_classes, ignore_label=255, output_dir=None)
+
+    def process(self, inputs, outputs):
+        """
+        Args:
+            inputs: the inputs to a model.
+                It is a list of dicts. Each dict corresponds to an image and
+                contains keys like "height", "width", "file_name".
+            outputs: the outputs of a model. It is either list of semantic segmentation predictions
+                (Tensor [H, W]) or list of dicts with key "sem_seg" that contains semantic
+                segmentation prediction in the same format.
+        """
+        for input, output in zip(inputs, outputs):
+            output = output["sem_seg"].argmax(dim=0).to(self._cpu_device)
+            pred = np.array(output, dtype=int)
+            with PathManager.open(self.input_file_to_gt_file[input["file_name"]], "rb") as f:
+                gt = np.array(Image.open(f), dtype=int)
+                if "category_colors" in input:
+                    gt = rgb2mask(gt,input["category_colors"]).astype(dtype=np.uint8)
+
+            gt[gt == self._ignore_label] = self._num_classes
+
+            self._conf_matrix += np.bincount(
+                self._N * pred.reshape(-1) + gt.reshape(-1), minlength=self._N ** 2
+            ).reshape(self._N, self._N)
+
+            self._predictions.extend(self.encode_json_sem_seg(pred, input["file_name"]))
 
 class MyMapper(DatasetMapper):
     def __init__(
@@ -98,8 +268,8 @@ class MyMapper(DatasetMapper):
         if "sem_seg_file_name" in dataset_dict:
             if "category_colors" in dataset_dict:
                 sem_seg_gt = utils.read_image(dataset_dict.pop("sem_seg_file_name"), "RGB")
-                sem_seg_gt = rgb2mask(sem_seg_gt, dataset_dict["category_colors"])
 
+                sem_seg_gt = rgb2mask(sem_seg_gt, dataset_dict["category_colors"])
 
             else:
                 sem_seg_gt = utils.read_image(dataset_dict.pop("sem_seg_file_name"), "L")
@@ -111,14 +281,12 @@ class MyMapper(DatasetMapper):
         aug_input = T.AugInput(image, sem_seg=sem_seg_gt)
         transforms = self.augmentations(aug_input)
         image, sem_seg_gt = aug_input.image, aug_input.sem_seg
-
-        image_shape = image.shape[:2]  # h, w
         # Pytorch's dataloader is efficient on torch.Tensor due to shared-memory,
         # but not efficient on large generic data structures due to the use of pickle & mp.Queue.
         # Therefore it's important to use torch.Tensor.
         dataset_dict["image"] = torch.as_tensor(np.ascontiguousarray(image.transpose(2, 0, 1)))
         if sem_seg_gt is not None:
-            dataset_dict["sem_seg"] = torch.as_tensor(sem_seg_gt.astype("long"))
+            dataset_dict["sem_seg"] = torch.as_tensor(sem_seg_gt,dtype=torch.long)
 
         if not self.is_train:
             # USER: Modify this if you want to keep them for some reason.
@@ -126,3 +294,16 @@ class MyMapper(DatasetMapper):
             return dataset_dict
         return dataset_dict
 
+def register_train_test(dataset_dict,metadata,test_ratio=0.33,seed=0):
+    DatasetCatalog.clear()
+    nb_input= len(dataset_dict)
+    x=np.arange(nb_input)
+    random.Random(seed).shuffle(x)
+    idx_split = int(len(x) * (1 - test_ratio))
+    DatasetCatalog.register("datasetTrain", my_dataset_function({"images":np.array(dataset_dict)[x[:idx_split]],"metadata":metadata}))
+    DatasetCatalog.register("datasetTest", my_dataset_function({"images":np.array(dataset_dict)[x[idx_split:]],"metadata":metadata}))
+    MetadataCatalog.get("datasetTrain").stuff_classes = [v for k,v in metadata["category_names"].items()]
+    MetadataCatalog.get("datasetTest").stuff_classes = [v for k,v in metadata["category_names"].items()]
+
+    """if ignoreValue is not None:
+        MetadataCatalog.get("datasetTrain").ignore_label = ignoreValue"""
