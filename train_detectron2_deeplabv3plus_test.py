b60208e75072ad23e524c71dd949de2690b65526
ambroise <ambroise.berthe@ikomia.com>
2022-06-20 17:42:01 +0200

    Add test file

---
+++ train_detectron2_deeplabv3plus_test.py
@@ -0,0 +1,17 @@
+import logging
+from ikomia.core import task
+from ikomia.utils.tests import run_for_test
+
+logger = logging.getLogger(__name__)
+
+
+def test(t, data_dict):
+    logger.info("===== Test::train detectron2 deeplabv3+ =====")
+    input_dataset = t.getInput(0)
+    params = task.get_parameters(t)
+    params["maxIter"] = 10
+    params["batchSize"] = 1
+    params["evalPeriod"] = 5
+    task.set_parameters(t, params)
+    input_dataset.load(data_dict["datasets"]["segmentation"]["dataset_wgisd"])
+    yield run_for_test(t)
