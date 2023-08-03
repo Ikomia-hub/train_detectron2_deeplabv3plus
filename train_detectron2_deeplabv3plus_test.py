de6fc762a437c5a94ed8a3eb0b1d2204690d6efa
ambroise <ambroise.berthe@ikomia.com>
2023-08-03 18:03:32 +0200

    Fix test file

--- train_detectron2_deeplabv3plus_test.py
+++ train_detectron2_deeplabv3plus_test.py
@@ -8,10 +8,10 @@ logger = logging.getLogger(__name__)
 def test(t, data_dict):
     logger.info("===== Test::train detectron2 deeplabv3+ =====")
     input_dataset = t.get_input(0)
-    params = task.get_parameters(t)
+    params = task.get_param_object()
     params["maxIter"] = 10
     params["batchSize"] = 1
     params["evalPeriod"] = 5
-    task.set_parameters(t, params)
+    t.set_parameters(params)
     input_dataset.load(data_dict["datasets"]["semantic_segmentation"]["dataset_coco"])
     yield run_for_test(t)
