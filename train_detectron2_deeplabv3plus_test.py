0d861daaece47ab4e0667e5bcff4b6fd48ca85e5
ambroise <ambroise.berthe@ikomia.com>
2023-08-07 16:00:36 +0200

    Fix test file

--- train_detectron2_deeplabv3plus_test.py
+++ train_detectron2_deeplabv3plus_test.py
@@ -1,5 +1,4 @@
 import logging
-from ikomia.core import task
 from ikomia.utils.tests import run_for_test
 
 logger = logging.getLogger(__name__)
@@ -8,10 +7,10 @@ logger = logging.getLogger(__name__)
 def test(t, data_dict):
     logger.info("===== Test::train detectron2 deeplabv3+ =====")
     input_dataset = t.get_input(0)
-    params = task.get_param_object()
-    params["maxIter"] = 10
-    params["batchSize"] = 1
-    params["evalPeriod"] = 5
+    params = t.get_parameters()
+    params["max_iter"] = "10"
+    params["batch_size"] = "1"
+    params["eval_period"] = "5"
     t.set_parameters(params)
     input_dataset.load(data_dict["datasets"]["semantic_segmentation"]["dataset_coco"])
     yield run_for_test(t)
